cd9e5bead11e2b90d504e120a0e37eb222088a72
RabbitDeWitt <davidsocoelho@outlook.com>
2023-09-16 16:36:36 -0300

    index.js limpo

--- components/index.js
+++
@@ -1,9 +0,0 @@
-import { Navbar } from '../../components/Navbar.js'
-import { Footer } from '../../components/Footer.js'
-import { Mensagem } from '../../components/Mensagem.js'
-
-export {
-  Navbar,
-  Footer,
-  Mensagem
-}
