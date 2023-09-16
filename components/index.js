3fd3336f6239fa10a7548bb22f9fe5d2c4b854e8
RabbitDeWitt <davidsocoelho@outlook.com>
2023-09-16 16:36:13 -0300

    novos arquivos

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import { Navbar } from '../../components/Navbar.js'
+import { Footer } from '../../components/Footer.js'
+import { Mensagem } from '../../components/Mensagem.js'
+
+export {
+  Navbar,
+  Footer,
+  Mensagem
+}
